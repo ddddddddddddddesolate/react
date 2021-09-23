4907badca5054f5985580edc1349d0c92be7ac56
Alexander <aleksandr.kalashnik@purrweb.com>
2021-09-23 14:24:57 +0600

    created file with posts data

---
+++ src/posts.js
@@ -0,0 +1,20 @@
+export const posts = [
+  {
+    id: 1,
+    title: "Post #1",
+    text: "Text for Post #1",
+    author: {
+      firstName: "John",
+      lastName: "Black"
+    }
+  },
+  {
+    id: 2,
+    title: "Post #2",
+    text: "Text for post #2",
+    author: {
+      firstName: "Walter",
+      lastName: "White"
+    }
+  }
+];
