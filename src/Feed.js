bd95ead954b09b8e3bd9f9dcf4fb779d47917519
Alexander <aleksandr.kalashnik@purrweb.com>
2021-09-23 14:24:40 +0600

    created feed component

---
+++ src/Feed.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+
+import Post from './Post'
+
+const Feed = ({ posts }) => {
+  return (
+    <Fragment>
+      <h1>Feed</h1>
+
+      { posts.map((post) => {
+        return <Post key={ post.id } { ...post } />
+      }) }
+    </Fragment>
+  );
+}
+
+export default Feed
