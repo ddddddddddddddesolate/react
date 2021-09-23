56cb63581bbd69db6a7c6a8750f62ef80f1ff05f
Alexander <aleksandr.kalashnik@purrweb.com>
2021-09-23 14:24:31 +0600

    created post component

---
+++ src/Post.js
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+
+import Author from './Author'
+
+const Post = ({ title, text, author }) => {
+  return (
+    <Fragment>
+      <h3>{ title }</h3>
+      <p>{ text }</p>
+
+      <Author { ...author } />
+    </Fragment>
+  );
+}
+
+export default Post
